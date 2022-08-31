f609c7af791134bd993d5068adcc37cbff5998b7
Brendan Ward <kingbeebop@gmail.com>
2022-08-30 20:14:55 -0400

    a

---
+++ public/precache-manifest.cdc3cfa5e7c8e0e0243ac04c7cb44e26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01cf87194f419d38e896",
+    "url": "/static/css/main.6fea1065.chunk.css"
+  },
+  {
+    "revision": "01cf87194f419d38e896",
+    "url": "/static/js/main.8b1c423e.chunk.js"
+  },
+  {
+    "revision": "31b6faa5ac5540836e4f",
+    "url": "/static/js/runtime~main.bb5fe24d.js"
+  },
+  {
+    "revision": "cb3ffae0f0aae6d358a7",
+    "url": "/static/js/2.9c9dd5cc.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "8e0bb0ec8c231de59bb81698fafd3fa8",
+    "url": "/static/media/Music.8e0bb0ec.png"
+  },
+  {
+    "revision": "724c5664c11d4b1f0d55bc39bd4b0981",
+    "url": "/index.html"
+  }
+];
