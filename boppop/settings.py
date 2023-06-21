b7e7c2014c078d45ccaf2d40f4d0f98b883f3d8f
kingbeebop <kingbeebop@gmail.com>
2023-06-21 11:44:14 -0400

    initial setup of models

--- boppop/settings.py
+++ boppop/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'boppop.Artist'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- boppop/views.py
+++ boppop/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from django.views.generic import RedirectView
+from social_django.utils import load_backend, load_strategy
+import os
+from dotenv import load_dotenv
 
 def index(request):
     return render(request, 'index.html')
