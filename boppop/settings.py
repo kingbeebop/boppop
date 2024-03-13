ea5de2243f4ffb663f002020ab354fd4bc571fd7
Brendan Ward <kingbeebop@gmail.com>
2024-03-13 14:15:50 -0400

    static files update

--- boppop/settings.py
+++ boppop/settings.py
@@ -158,11 +158,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "app/build/static")
+# ]
+STATIC_URL = '/static/'
+
+# Additional directories where Django will look for static files during development.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app/build/static")
+    os.path.join(BASE_DIR, 'boppop/static'),
 ]
+
+# Directory where collected static files will be stored for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
