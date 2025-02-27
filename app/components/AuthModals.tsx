3dd212bc67fe9b95c4d030598cd1f2db7f81897f
Bop <kingbeebop@gmail.com>
2025-02-26 20:23:01 -0500

    dockerfilee update

--- app/components/AuthModals.tsx
+++ app/components/AuthModals.tsx
@@ -1,1 +1,13 @@
- 
+import React from 'react';
+// Add your other imports here
+
+// Your component code here...
+
+export {}; // Add this if the file doesn't export anything else
+
+// If you have a component, export it:
+// export const AuthModals: React.FC = () => {
+//   return (
+//     // Your component JSX
+//   );
+// }; 
