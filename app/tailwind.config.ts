8fa3e0a9a1b93a7729d13fec3fbd55ead731506f
Bop <kingbeebop@gmail.com>
2025-02-27 10:55:09 -0500

    remove fix ts

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,5 +1,21 @@
 // tailwind.config.js
 
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
+
 // module.exports = {
 //   content: [
 //     './src/**/*.{js,ts,jsx,tsx}',
